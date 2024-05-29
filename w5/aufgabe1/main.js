7b68ca2041cb1189bc1fc62690d94e9c10f8e138
Lukas Bucher <lukas.bucherr@gmail.com>
2024-05-29 10:26:36 +0200

    birdo cube <3

--- w5/aufgabe1/main.js
+++ w5/aufgabe1/main.js
@@ -119,7 +119,7 @@ function main(gl, sp) {
     let uModelViewLoc = gl.getUniformLocation(sp, "uModelView")
     Mat.set(gl, uModelViewLoc, mvw)
 
-    gl.clearColor(1.0, 1.0, 0.0, 1.0)
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.enable(gl.CULL_FACE)
 
--- w5/aufgabe2/main.js
+++ w5/aufgabe2/main.js
@@ -26,9 +26,6 @@ function main() {
         return;
     }
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
     const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
     const programInfo = {
